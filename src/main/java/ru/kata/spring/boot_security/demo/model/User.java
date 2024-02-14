05b73d951720e0baa0981454f4c213bcd09d5f3a
Екатерина Минеева <katemineeva07@gmail.com>
2024-02-14 13:47:53 +0300

    token mistake

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -1,10 +1,7 @@
 package ru.kata.spring.boot_security.demo.model;
 
-
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
